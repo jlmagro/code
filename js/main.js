396b49f15f629e46242c9a77c8dc01b360d31427
josemagro <josemagro1989@hotmail.com>
2018-05-27 13:19:04 +0100

    inicio da alteração à página "work"

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
+$(function() {
+
+    var welcomeSection = $('.welcome-section'),
+        enterButton = welcomeSection.find('.enter-button');
+
+    setTimeout(function() {
+        welcomeSection.removeClass('content-hidden');
+    }, 500);
+
+    enterButton.on('click', function(e){
+        e.preventDefault();
+            welcomeSection.addClass('content-hidden').fadeOut(300);
+            $('.animsition').removeClass('container-hidden').fadeIn(600);
+    });
+
+
+});
+
+$(document).ready(function() {
+    $('.animsition').animsition();
+});
+
 //animação das setas
 $(function() {
     setTimeout('downArrow()');
 });
 
+//animação das setas
 function downArrow() {
     $('.downArrow').animate({
         marginBottom: '-=10px'
@@ -10,35 +33,6 @@ function downArrow() {
         marginBottom: '+=10px'
     }, 400);
     setTimeout('downArrow()', 700);
-}
-
-//include HTML files in html
-function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /*loop through a collection of all HTML elements:*/
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        /*search for elements with a certain atrribute:*/
-        file = elmnt.getAttribute("include-html");
-        if (file) {
-            /*make an HTTP request using the attribute value as the file name:*/
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4) {
-                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-                    /*remove the attribute, and call this function once more:*/
-                    elmnt.removeAttribute("include-html");
-                    includeHTML();
-                }
-            }
-            xhttp.open("GET", file, true);
-            xhttp.send();
-            /*exit the function:*/
-            return;
-        }
-    }
 }
 
 //animação das barras da foto
@@ -49,6 +43,7 @@ sr.reveal('.box3', { duration: 700 });
 sr.reveal('.box4', { duration: 800 });
 sr.reveal('.box5', { duration: 600 });
 
+//configuração do email
 function sendEmail()
 {
     window.location = "josemagro1989@icloud.com";
--- js/transitions.js
+++ js/transitions.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
     $(".animsition").animsition({
-        inClass: 'zoom-in',
-        outClass: 'zoom-out',
-        inDuration: 1500,
+        inClass: 'fade-in',
+        outClass: 'fade-out',
+        inDuration: 3000,
         outDuration: 800,
         linkElement: '.animsition-link',
         // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
-        loading: true,
+        loading: false,
         loadingParentElement: 'body', //animsition wrapper element
-        loadingClass: 'animsition-loading',
+        loadingClass: '.animsition-loading',
         loadingInner: '', // e.g '<img src="loading.svg" />'
-        timeout: true,
+        timeout: false,
         timeoutCountdown: 5000,
         onLoadEvent: true,
         browser: [ 'animation-duration', '-webkit-animation-duration'],
